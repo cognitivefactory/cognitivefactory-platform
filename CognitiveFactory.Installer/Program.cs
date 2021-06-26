323410758deed33eaffe7e3a51d0e48470d779d7
laurent prud'hon <laurent.prudhon@hotmail.fr>
2021-06-26 16:36:06 +0200

    Console test OK

---
+++ CognitiveFactory.Installer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Spectre.Console;
+using System;
+
+namespace CognitiveFactory.Installer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.Render(new BarChart()
+                .Width(60)
+                .Label("[green bold underline]Number of fruits[/]")
+                .CenterLabel()
+                .AddItem("Apple", 12, Color.Yellow)
+                .AddItem("Orange", 54, Color.Green)
+                .AddItem("Banana", 33, Color.Red));
+            Console.ReadLine();
+        }
+    }
+}
