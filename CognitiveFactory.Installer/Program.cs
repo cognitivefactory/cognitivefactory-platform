22d280b23f2f823484a9a61a585c51b10219f292
laurent prud'hon <laurent.prudhon@hotmail.fr>
2021-06-27 17:09:00 +0200

    PlatformDetection

--- CognitiveFactory.Installer/Program.cs
+++ CognitiveFactory.Installer/Program.cs
@@ -6,13 +6,11 @@ namespace CognitiveFactory.Installer
 {
     class Program
     {
-        public static string Version = "0.1";
-
         static int Main(string[] args)
         {
-            AnsiConsole.MarkupLine(              "[bold navy on paleturquoise1]--------------------------------------------[/]");
-            AnsiConsole.MarkupLine(String.Format("[bold navy on paleturquoise1]| CognitiveFactory Platform Installer v{0} |[/]", Version));
-            AnsiConsole.MarkupLine(              "[bold navy on paleturquoise1]--------------------------------------------[/]");
+            AnsiConsole.MarkupLine("[bold navy on paleturquoise1]---------------------------------------[/]");
+            AnsiConsole.MarkupLine("[bold navy on paleturquoise1]| CognitiveFactory Platform Installer |[/]");
+            AnsiConsole.MarkupLine("[bold navy on paleturquoise1]---------------------------------------[/]");
             AnsiConsole.WriteLine();
 
             AnsiConsole.MarkupLine("1. [underline]Checking operating system[/] :");
@@ -21,16 +19,11 @@ namespace CognitiveFactory.Installer
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {                
                 try
-                {
-                    var winver = Environment.OSVersion.Version;
-                    if (winver.Major < 10)
+                {                    
+                    if (!PlatformDetection.IsWindows10Version1903OrGreater)
                     {
-                        AnsiConsole.MarkupLine(String.Format("[red]Sorry, Windows 10 is required[/] (your version = \"{0}\")", winver));
-                        return 1;
-                    }
-                    else if(winver.Build < 18362)
-                    {
-                        AnsiConsole.MarkupLine(String.Format("[red]Sorry, Windows 10 version 1903 (build number > 18362) is required[/] (your build = \"{0}\")", winver));
+                        var winver = Environment.OSVersion.Version;
+                        AnsiConsole.MarkupLine(String.Format("[red]Sorry, Windows 10 version 1903 (build number > 18362) is required[/] (your version = \"{0}\")", winver));
                         return 1;
                     } 
                     else
@@ -51,18 +44,28 @@ namespace CognitiveFactory.Installer
                 // https://github.com/dotnet/core-setup/blob/master/src/managed/Microsoft.DotNet.PlatformAbstractions/Native/NativeMethods.Unix.cs
 
                 try
-                {
-                    var linuxver = Environment.OSVersion.Version;
-                    if (linuxver.Major < 5 || (linuxver.Major == 5 && linuxver.Minor < 4))
+                {                    
+                    if (!PlatformDetection.IsUbuntu1804OrHigher)                            
                     {
-                        AnsiConsole.MarkupLine(String.Format("[red]Sorry, Linux kernel version > 5.4 is required[/] (your version = \"{0}\")", linuxver));
+                        var linuxdistro = PlatformDetection.GetDistroInfo();
+                        AnsiConsole.MarkupLine(String.Format("[red]Sorry, Ubuntu version 1804 is required[/] (your distribution = \"{0} {1}\")", linuxdistro.Id, linuxdistro.VersionId));
                         return 1;
                     }
                     else
                     {
-                        AnsiConsole.MarkupLine("Linux kernel version [bold green]OK[/]");
-                        AnsiConsole.WriteLine();
-                        DoLinuxInstall();
+                        var linuxver = Environment.OSVersion.Version;
+                        if (linuxver.Major < 5 || (linuxver.Major == 5 && linuxver.Minor < 4))
+                        {
+                            AnsiConsole.MarkupLine(String.Format("[red]Sorry, Linux kernel version > 5.4 is required[/] (your version = \"{0}\")", linuxver));
+                            return 1;
+                        }
+                        else
+                        {
+
+                            AnsiConsole.MarkupLine("Linux version [bold green]OK[/]");
+                            AnsiConsole.WriteLine();
+                            DoLinuxInstall();
+                        }
                     }
                 }
                 catch (Exception e)
@@ -85,6 +88,9 @@ namespace CognitiveFactory.Installer
                 AnsiConsole.MarkupLine("[red]Sorry, your operating system is not supported[/]");
                 return 1;
             }
+
+            Console.WriteLine($"Running as admin ? {AdminHelpers.IsProcessElevated()}");
+            Console.ReadLine();
 
             return 0;
         }
